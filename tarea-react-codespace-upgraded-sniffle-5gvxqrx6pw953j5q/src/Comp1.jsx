7872bd71744aa2a80a7f9974f3e0bfce862fcc11
vda2511 <161495463+vda2511@users.noreply.github.com>
2024-06-11 13:07:37 -0400

    Add files via upload

---
+++ tarea-react-codespace-upgraded-sniffle-5gvxqrx6pw953j5q/src/Comp1.jsx
@@ -0,0 +1,10 @@
+function Hola(){
+    return(
+        <div className="abc">
+        <h1 className="abcd">saludando juan perez</h1>
+            <p>hola mundo jejejejej</p>
+        </div>
+        
+    );
+}
+export default Hola;
---
+++ tarea-react-codespace-upgraded-sniffle-5gvxqrx6pw953j5q/src/Tarea.jsx
@@ -0,0 +1,14 @@
+function Tarea(){
+    return (
+        <center><div className="cont1">
+          <div className="cont2">
+            <h1 className="cont3">Diego Huallpa Mamani</h1>
+            <p className="cont3">Hola mundo este es un contenedor de texto e imagene creado con React</p>
+          </div>
+          <div className="image-container">
+            <img src="src/img/1200_800.jpeg" alt="Descripción de la imagen" />
+          </div>
+        </div></center>
+      );
+}
+export default Tarea;
